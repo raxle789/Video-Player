3588c8810297edaf4d03a0a5c3c76476b12ab2a7
raxle789 <isqdifatih182@gmail.com>
2023-05-30 10:51:29 +0700

    commit pertama setelah dibuat

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
+import cassettePng from './assets/cassette.png';
+import cassetteGif from './assets/cassette.gif';
+import upload from './assets/upload.gif';
+import background from './assets/background.png'
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {const [videoSource, setVideoSource] = useState(null);
+  const [videoTitle, setVideoTitle] = useState('No video playing');
+
+  const uploadVideo = (e) => {
+    let file = e.target.files[0];
+    if(file !== undefined) {
+      let url = URL.createObjectURL(file);
+      setVideoSource(url);
+      setVideoTitle(file.name);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className="app-header">
+        <h1 className='logo'>Video Player</h1>
+        <div className="video-title-container">
+          {videoTitle === 'No video playing' ? <p className='video-title'>{videoTitle}</p> : <marquee className='video-title' direction='left' scrollamount="4">{videoTitle}</marquee>}          
+          {videoSource === null ? <img src={cassettePng} className='cassette' alt='cassette-icon' /> : <img src={cassetteGif} className='cassette' alt='cassette-icon' />}
+        </div>
       </header>
-    </div>
+      <main>
+        <section className='left-main'>
+          <h2 className='desc1'>Upload and Watch</h2>
+          <h2 className='desc2'>Your Video</h2>
+          <div className='upload-container'>
+            <input className='input-btn' type="file" id="video-input" name="video" accept="video/*" onChange={uploadVideo} />
+            <label htmlFor="video-input">
+              <img src={upload} className='upload-icon' alt='upload' />
+              <p className='upload-label'>Upload Video</p>
+            </label>
+          </div>
+        </section>
+        <section className='right-main'>
+          <div className='video-player-wrapper'>
+            <div className='video-player-container'>
+              <video className='video-player' src={videoSource} controls autoPlay />
+            </div>
+          </div>
+        </section>
+      </main>
+      <div className='gradient1'></div>
+      <img src={background} className='background' alt='background' />
+      <div className='gradient2'></div>
+    </>
   );
 }
 
