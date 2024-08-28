df6a1dda9e66d928aa6997263b9ce258b84916c0
raxle789 <isqdifatih182@gmail.com>
2024-08-28 14:05:39 +0700

    fix: memperbaiki responsive design pt 2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import cassettePng from './assets/cassette.png';
-import cassetteGif from './assets/cassette.gif';
-import upload from './assets/upload.gif';
-import background from './assets/background.png'
-import { useState } from 'react';
-import './App.css';
+import cassettePng from "./assets/cassette.png";
+import cassetteGif from "./assets/cassette.gif";
+import upload from "./assets/upload.gif";
+import background from "./assets/background.png";
+import { useState } from "react";
+import "./App.css";
 
-const App = () => {const [videoSource, setVideoSource] = useState(null);
-  const [videoTitle, setVideoTitle] = useState('No video playing');
+const App = () => {
+  const [videoSource, setVideoSource] = useState(null);
+  const [videoTitle, setVideoTitle] = useState("No video playing");
 
   const uploadVideo = (e) => {
     let file = e.target.files[0];
-    if(file !== undefined) {
+    if (file !== undefined) {
       let url = URL.createObjectURL(file);
       setVideoSource(url);
       setVideoTitle(file.name);
@@ -19,37 +20,59 @@ const App = () => {const [videoSource, setVideoSource] = useState(null);
   return (
     <>
       <header className="app-header">
-        <h1 className='logo'>Video Player</h1>
+        <h1 className="logo">Video Player</h1>
         <div className="video-title-container">
-          {videoTitle === 'No video playing' ? <p className='video-title'>{videoTitle}</p> : <marquee className='video-title' direction='left' scrollamount="4">{videoTitle}</marquee>}          
-          {videoSource === null ? <img src={cassettePng} className='cassette' alt='cassette-icon' /> : <img src={cassetteGif} className='cassette' alt='cassette-icon' />}
+          {videoTitle === "No video playing" ? (
+            <p className="video-title">{videoTitle}</p>
+          ) : (
+            <marquee className="video-title" direction="left" scrollamount="4">
+              {videoTitle}
+            </marquee>
+          )}
+          {videoSource === null ? (
+            <img src={cassettePng} className="cassette" alt="cassette-icon" />
+          ) : (
+            <img src={cassetteGif} className="cassette" alt="cassette-icon" />
+          )}
         </div>
       </header>
       <main>
-        <section className='left-main'>
-          <h2 className='desc1'>Upload and Watch</h2>
-          <h2 className='desc2'>Your Video</h2>
-          <div className='upload-container'>
-            <input className='input-btn' type="file" id="video-input" name="video" accept="video/*" onChange={uploadVideo} />
+        <section className="left-main">
+          <h2 className="desc1">Upload and Watch</h2>
+          <h2 className="desc2">Your Video</h2>
+          <div className="upload-container">
+            <input
+              className="input-btn"
+              type="file"
+              id="video-input"
+              name="video"
+              accept="video/*"
+              onChange={uploadVideo}
+            />
             <label htmlFor="video-input">
-              <img src={upload} className='upload-icon' alt='upload' />
-              <p className='upload-label'>Upload Video</p>
+              <img src={upload} className="upload-icon" alt="upload" />
+              <p className="upload-label">Upload Video</p>
             </label>
           </div>
         </section>
-        <section className='right-main'>
-          <div className='video-player-wrapper'>
-            <div className='video-player-container'>
-              <video className='video-player' src={videoSource} controls autoPlay />
+        <section className="right-main">
+          <div className="video-player-wrapper">
+            <div className="video-player-container">
+              <video
+                className="video-player"
+                src={videoSource}
+                controls
+                autoPlay
+              />
             </div>
           </div>
         </section>
       </main>
-      <div className='gradient1'></div>
-      <img src={background} className='background' alt='background' />
-      <div className='gradient2'></div>
+      <div className="gradient1"></div>
+      <img src={background} className="background" alt="background" />
+      <div className="gradient2"></div>
     </>
   );
-}
+};
 
 export default App;
